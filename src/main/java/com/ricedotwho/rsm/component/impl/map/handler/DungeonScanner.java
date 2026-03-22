0e6e1fe74d6de0f5dde9aff8f4fc60db334d2e13
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-03-22 17:05:03 +1100

    🙏

--- src/main/java/com/ricedotwho/rsm/component/impl/map/handler/DungeonScanner.java
+++ src/main/java/com/ricedotwho/rsm/component/impl/map/handler/DungeonScanner.java
@@ -43,7 +43,7 @@ public class DungeonScanner implements Accessor {
     }
 
     public boolean shouldScan() {
-        return !isScanning && !hasScanned && System.currentTimeMillis() - lastScanTime >= 250 && Location.getFloor() != Floor.None;
+        return !isScanning && !hasScanned && System.currentTimeMillis() - lastScanTime >= 250 && Location.getFloor() != Floor.None && Dungeon.isStarted();
     }
 
     public void scan() {
@@ -77,7 +77,7 @@ public class DungeonScanner implements Accessor {
             }
         }
 
-        if (allChunksLoaded && DungeonInfo.getUniqueRooms().stream().noneMatch(r  -> r.getRotation().equals(RoomRotation.UNKNOWN))) {
+        if (Dungeon.isStarted() && allChunksLoaded && DungeonInfo.getUniqueRooms().stream().noneMatch(r  -> r.getRotation().equals(RoomRotation.UNKNOWN))) {
             DungeonInfo.setRoomCount(((int) Arrays.stream(DungeonInfo.getDungeonList()).filter(tile -> tile instanceof Room && !((Room) tile).isSeparator()).count()));;
             hasScanned = true;
             new DungeonEvent.ScanComplete().post();
