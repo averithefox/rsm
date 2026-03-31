0671c563648d71c362f45ffaff18fd2f8ceb6d22
SicoKaleb <Sicokaleb1324@gmail.com>
2026-03-30 22:37:50 -0500

    Opsec modue (NickHider, ServerIdHider)

--- src/main/java/com/ricedotwho/rsm/RSM.java
+++ src/main/java/com/ricedotwho/rsm/RSM.java
@@ -31,6 +31,7 @@ import com.ricedotwho.rsm.module.impl.player.keyshortcuts.KeyShortcuts;
 import com.ricedotwho.rsm.module.impl.render.*;
 import com.ricedotwho.rsm.module.impl.render.hud.Hud;
 import com.ricedotwho.rsm.module.impl.render.ImageHud;
+import com.ricedotwho.rsm.module.impl.render.opsec.OpSec;
 import com.ricedotwho.rsm.module.impl.render.visualwords.VisualWords;
 import com.ricedotwho.rsm.ui.clickgui.RSMConfig;
 import com.ricedotwho.rsm.ui.clickgui.RSMGuiEditor;
@@ -107,6 +108,7 @@ public class RSM implements ClientModInitializer {
             ChestHitFix.class,
             P3Qol.class,
             VisualWords.class,
+            OpSec.class,
             Hud.class,
             ImageHud.class,
             KeyShortcuts.class,
